64675c0c9e5d4e4efa949442c0a76a9cc088843d
insumanth <sumanthreddystar@gmail.com>
2021-10-16 15:26:08 +0000

    Started def read_images() function
    1. Stared def read_images() function with basic code
    2. Added 'others/' to .gitignore file

--- batchimage/__init__.py
+++ batchimage/__init__.py
@@ -3,3 +3,9 @@
 __author__ = """Sumanth"""
 __email__ = 'sumanthreddystar@gmail.com'
 __version__ = '0.1.0'
+
+
+
+# Imports
+
+from .batchimage import image_processing
--- batchimage/batchimage.py
+++ batchimage/batchimage.py
@@ -1,1 +1,22 @@
 """Main module."""
+
+# Imports
+from pathlib import Path
+
+def read_images(path, file_extension = []):
+
+    file_names = []
+
+    if file_extension == []:
+        extensions = ['.jpg', '.png', '.bmp', '.pbm', '.pgm', '.ppm', '.sr', '.ras', '.jpeg', '.jpe', '.jp2', '.tiff', '.tif']
+    else:
+        extensions = file_extension
+    
+
+    entries = Path(path)
+
+    for entry in entries.iterdir():
+        if entry.is_file() and entry.name.endswith(tuple(extensions)):
+            file_names.append(entry.name)
+
+    return file_names
